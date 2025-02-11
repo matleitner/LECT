ad284d131d70cb1290d708841feb3bd7d87c4f62
martimleitner <leitnermartim@ua.pt>
2025-02-11 21:08:34 +0000

    Leitner

---
+++ 1º_ano/1º_semestre/IEI/html/JavaScript/dom.js
@@ -0,0 +1,7 @@
+var x = document.getElementById( "op1" );
+var y = document.getElementById( "op2" );
+console.log( parseFloat(x.value) );
+console.log( parseFloat(y.value) );
+var r = parseFloat(x.value) +parseFloat(y.value)
+
+
