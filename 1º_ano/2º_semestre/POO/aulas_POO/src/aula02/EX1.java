afe089750ccc9549e9aaf07d9f61eb8d26333a3c
martimleitner <leitnermartim@ua.pt>
2025-02-12 19:21:10 +0000

    Leitner

---
+++ 1º_ano/2º_semestre/POO/aulas_POO/src/aula02/EX1.java
@@ -0,0 +1,9 @@
+package aula02;
+
+public class EX1 {
+    public static void main(String[] args) {
+        for (int i=0, i<10, i++ )
+            System.out.println("Hello");
+
+    }
+}
---
+++ 1º_ano/2º_semestre/POO/primeiro_proj/Hello.java
@@ -0,0 +1,6 @@
+package primeiro_proj;
+public class Hello { 
+    public static void main(String[] args) { 
+     System.out.println("Hello, World!"); 
+    } 
+} 
