d4357faaa3b68d87039a223f0a76a63d938b5eb3
martimleitner <leitnermartim@ua.pt>
2025-02-26 10:35:55 +0000

    Leitner

---
+++ 1º_ano/2º_semestre/POO/aulas_POO/src/aula03/Ex3_1.java
@@ -0,0 +1,20 @@
+package aula03;
+import java.util.Scanner;
+
+public class Ex3_1 {
+    public static void main(String[] args){
+
+        int numeroInteiro;
+        
+    }
+    public static int NumeroInteiroPositivo(int numeroInteiro){
+            Scanner sc = new Scanner(System.in);
+            
+            sc.close();
+            
+
+            
+            return numeroInteiro;
+        }
+}
+
