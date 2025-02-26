79504a09497b2b9a028258a7052039ae8aa848b6
matleitner <leitnermartim@gmail.com>
2025-02-26 21:39:52 +0000

    Leitner

---
+++ 1º_ano/2º_semestre/LI/guiao_2/factorial.py
@@ -0,0 +1,18 @@
+def factorial(x):
+    if x == 0:
+        return 1
+    if x == 1:
+        return 1
+    reg = 1
+    
+    while x>1:
+        reg*=x
+        x-=1
+
+    return reg
+
+def main():
+    print( factorial(-1))
+
+if __name__ == "__main__":
+    main()
