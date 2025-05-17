097ced2acf7a492c6fb65d2d904d2ac9b31b5d94
matleitner <leitnermartim@gmail.com>
2025-05-17 22:04:29 +0100

    leitner

--- 1º_ano/2º_semestre/POO/aulas_POO/src/aula10/Ex10_4/Voo.java
+++ 1º_ano/2º_semestre/POO/aulas_POO/src/aula10/Ex10_4/Voo.java
@@ -8,45 +8,52 @@ import java.util.Arrays;
 import java.util.TreeMap;
 import java.util.ArrayList;
 public class Voo {
-    // String[] infoVoos = new String[6];
-    public static ArrayList<Object> load(){
-        ArrayList<String> voosTodos = new ArrayList<>();
+    
+    public static ArrayList<ArrayList<String>> load(){
+        TreeMap<String, String> comp = companhia();
+        ArrayList<ArrayList<String>> voosTodos= new ArrayList<>(); 
+
         try(Scanner sc = new Scanner(new File("voos.txt")))
-        
-        {   sc.nextLine();
+        {  sc.nextLine();
             while(sc.hasNextLine()){
-            ArrayList<String> voos = new ArrayList<>();
-            String[] infoVoos = sc.nextLine().replace(" ", "").split("\t");
-            Arrays.stream(infoVoos).forEach(n->voos.add(n));
-       
-            TreeMap<String, String> comp = companhia();
-            
-            comp.forEach((sigla,companhia) -> {
-                if(sigla.equals(infoVoos[1].substring(0, 2))){
-
+                ArrayList<String> voos = new ArrayList<>();
+                String[] infoVoos = sc.nextLine().trim().replace(",", "|").split("\t");
+                Arrays.stream(infoVoos).forEach(n->voos.add(n));
+                String sigla = infoVoos[1].substring(0,2);
+                String companhia = comp.getOrDefault(sigla, "Desconhecido");
+               
                 voos.add(companhia);
+                if(voos.size()==5){
+                    voos.add( sumHour(voos.get(0), voos.get(3)));
+   
+                }
 
+
+                voosTodos.add(voos);
+                }         
+                
             }
-        });
-
+                
             //  HORA VOO ORGIEM ATRASO (OPCIONAL) 
             
-        }
-
-        }catch(IOException e){
+        
+    
+        catch(IOException e){
             e.printStackTrace();
         }
-
-        return voos;
+        
+        return voosTodos;
     }
+
+
     public static String sumHour(String hora1, String hora2){
         //formato Hora:Min
         String[] hora_minuto1 = hora1.split(":");
         String[] hora_minuto2= hora2.split(":");
-        int extraHour = (Integer.parseInt(hora_minuto1[1])+Integer.parseInt(hora_minuto2[0]))/60;
+        int extraHour = (Integer.parseInt(hora_minuto1[1])+Integer.parseInt(hora_minuto2[1]))/60;
         int horaFinal = (Integer.parseInt(hora_minuto1[0]) + Integer.parseInt(hora_minuto2[0])+extraHour)%24;
-        int minutoFinal = (Integer.parseInt(hora_minuto1[1])+Integer.parseInt(hora_minuto2[0]))%60; 
-        return horaFinal + ":" + minutoFinal;
+        int minutoFinal = (Integer.parseInt(hora_minuto1[1])+Integer.parseInt(hora_minuto2[1]))%60; 
+        return String.format("%02d",horaFinal) + ":" +String.format("%02d", minutoFinal);
     }
 
 
@@ -65,12 +72,31 @@ public class Voo {
         }catch(IOException e){
             e.printStackTrace();
         }
+    
         return comp;
     }
-    public static void main(String[] args){
-        System.out.println("Hora\tVoo\tCompanhia\tOrigem\tAtraso\tObs");
+
+
+
+    public static void printFlights(){
+        ArrayList<ArrayList<String>> voosTodos = load();
+        for(ArrayList<String> voo: voosTodos){
+            if(voo.size()==6)
+            System.out.printf("%-30s%-30s%-30s%-30s%-30s%-30s\n", voo.get(0), voo.get(1), voo.get(4),voo.get(2),voo.get(3), voo.get(5));
+            else
+            System.out.printf("%-30s%-30s%-30s%-30s\n", voo.get(0), voo.get(1), voo.get(3),voo.get(2));
         
-        System.out.println(load().toString());
+        }    
+    
+    
+    }
+
+
+   
+    public static void main(String[] args){
+        System.out.printf("%-30s%-30s%-30s%-30s%-30s%-30s\n\n","Hora","Voo","Companhia","Origem","Atraso","Obs");
+//        load().forEach(System.out::println);
+         printFlights();
 
     }
 }
