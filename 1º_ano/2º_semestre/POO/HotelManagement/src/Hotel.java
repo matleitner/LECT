dd4f66e9d6e22783a45869eb7bb777ebc5fd10b0
martimleitner <leitnermartim@ua.pt>
2025-05-27 19:47:22 +0100

    Leitner

--- 1º_ano/2º_semestre/POO/HotelManagement/src/Hotel.java
+++ 1º_ano/2º_semestre/POO/HotelManagement/src/Hotel.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+
 public class Hotel {
+    private ArrayList<Rooms> roomList; 
+
+
+    public Hotel(){
+        roomList = new ArrayList<>();
+    }
+    public void addRoom(Rooms room){
+        roomList.add(room);
+    }
+    
+    
     
 }
--- 1º_ano/2º_semestre/POO/HotelManagement/src/Reserva.java
+++ 1º_ano/2º_semestre/POO/HotelManagement/src/Reserva.java
@@ -1,8 +1,15 @@
+import java.time.LocalDate;
+
 public class Reserva {
+    private Clients cliente;
+    private LocalDate dataEntrada;
+    private LocalDate dataSaida;
+    private Rooms quarto;
+    private int codigo;
+
     
     
-    
-    public Reserva(int cc){
+    public Reserva(int codigo, Clients cliente, LocalDate dataEntrada, LocalDate data){
         
 
     }
