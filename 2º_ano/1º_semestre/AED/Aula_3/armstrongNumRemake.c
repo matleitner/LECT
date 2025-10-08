40be19fc7d05162f71b6370262b2e11a161016a9
matleitner <testembl04@gmail.com>
2025-10-08 10:54:24 +0100

    Aula 4 de aed pratica

--- 2º_ano/1º_semestre/AED/Aula_3/armstrongNumRemake.c
+++ 2º_ano/1º_semestre/AED/Aula_3/armstrongNumRemake.c
@@ -1,1 +1,39 @@
 #include <stdio.h>
+#include <math.h>
+
+
+
+int main(void){
+	int num;
+	int numero = 1;
+	scanf("%d\n", &num);
+	int potencias[10];
+	for(int i = 0; i<=9; i++){
+		int p = 1;
+		for(int j = 0; j<num;j++){
+			p *= i; 
+		}
+			
+		potencias[i] = p;
+	}
+	for (int m = 0; m<num;m++){
+		numero*=10;
+	
+	}
+	
+	for(int j = 1; j<numero;j++){
+		int sum = 0;
+		int temp = j;
+	
+		while(temp>0){
+			int dig = temp %10;
+			sum+= potencias[dig]; 
+			temp/=10;;
+			}	
+		if(sum == num) printf("%d\n",j); 	
+	}
+
+	return 0;	
+	
+	
+}
