90aee5b75946a8821a7e2337e5811fafb74908f9
matleitner <testembl04@gmail.com>
2025-12-20 00:01:26 +0000

    Teste 2023 AC P2 feito

--- 2º_ano/1º_semestre/AC-I/Teste_Exemplo2/Exame-AC1-P2-2023/ex2.asm
+++ 2º_ano/1º_semestre/AC-I/Teste_Exemplo2/Exame-AC1-P2-2023/ex2.asm
@@ -1,72 +1,141 @@
-#float proc(double *array, double thd, double val, int n) {
+ #float proc(double *array, double thd, double val, int n) {
+
 #    int i;
+
 #    double aux, sum;
+
 #
+
 #    sum = 0.0;
+
 #    for (i = 0; i < n; i++) {
+
 #        aux = array[i] + val;
+
 #        if (aux > thd) {
+
 #            array[i] = thd;
+
 #            sum += thd;
+
 #        } else {
+
 #            array[i] = aux;
+
 #            sum += aux;
+
 #        }
+
 #    }
+
 #    return (float) (sum / (double) n);
+
 #}
 
-# array: $t0	
-# thd:	 $f2
-# val:	 $f4
-# n:	 $t1
-# i:	 $t2
-# aux:	 $f6
-# sum:	 $f8
 
-	.data
-	
-zero:	.double	0.0
-	
-	.text
-	.globl	proc	
-proc:					# float proc(double *array, double thd, double val, int n) {
-	la	$t0, zero
-	l.d	$f8, 0($t0)		# sum = 0.0;
-	
-	move	$t0, $a0		# double *array;
-	mov.d	$f2, $f12		# double thd;
-	mov.d	$f4, $f14		# double val;
-	move	$t1, $a1		# int n;
-	
-	li	$t2, 0			# i = 0;
-for:
-	bge	$t2, $t1, endfor	# if(i >= n) goto endfor;
-	
-	sll	$t3, $t2, 3		# $t3 = 8 * i;
-	addu	$t3, $t0, $t3		# $t3 = &array[i];
-	l.d	$f10, 0($t3)		# $f10 = array[i];
-	
-	add.d	$f6, $f10, $f4		# aux = array[i] + val;
-	
-if:
-	c.le.d	$f6, $f2		# if (thd >= aux) <=> if (aux <= thd)
-	bc1t	 else			# goto else;
-	
-	s.d	$f2, 0($t3)		# array[i] = thd;
-	add.d	$f8, $f8, $f2		# sum += thd;
-	
-	j 	endif
-else:
-	s.d	$f6, 0($t3)		# array[i] = aux;
-	add.d	$f8, $f8, $f6		# sum += aux;
-endif:
-	addi	$t2, $t2, 1		# i++;
-	j 	for
+# array: $t0    
+
+# thd:     $f12
+
+# val:     $f14
+
+# n:     $t1
+
+# i:     $t2
+
+# aux:     $f6
+
+# sum:     $f8
+
+
+
+    .data
+
+
+sum:    .double 0.0
+
+    
+
+    .text
+
+    .globl main    
+
+    
+
+proc:    or $t0, $0, $a0
+
+    or $t1, $0, $a1
+
+    la $t2, sum
+
+    l.d $f8, 0($t2)
+
+    
+
+    li $t2, 0        # i = 0 
+
+    
+
+for:    bge $t2, $t1, endfor    #for (i = 0; i < n; i++) {
+
+    
+
+    sll $t3, $t2, 3        # i * 8
+
+    addu $t3, $a0, $t3    # $t3 = &array[i]          
+
+    
+
+    l.d $f10, 0($t3)        # f10 = array[i]
+
+    add.d $f6, $f10, $f14    # aux = array[i] + val
+
+    
+
+if:    c.le.d $f6, $f12        # if(aux > thd)
+
+    bc1t else        #
+
+    
+
+    s.d $f12, 0($t3)        # array[i] = thd
+
+    add.d $f8, $f8, $f12    # sum += thd;
+
+    j endif
+
+    
+
+else:    s.d $f6, 0($t3)        # array[i] = aux
+
+    add.d $f8, $f8, $f6    # sum += aux;
+
+endif:        
+
+    addi $t2, $t2, 1
+
+    j for
+
 endfor:
-	mtc1	$t1, $f16
-	cvt.d.w	$f16, $f16		# $f16 = (double) n;
-	div.d	$f0, $f8, $f16		# $f0 = sum / (double) n
-	cvt.s.d	$f0, $f0		# $f0 = (float) $f0
-	
-	jr	$ra
+
+    mtc1 $a1, $f10        #
+
+    cvt.d.w $f10, $f10    #
+
+    div.d $f0, $f8, $f10    #
+
+    cvt.s.d $f0, $f0        # return (float) (sum/ (double) n);
+
+    
+
+    jr $ra
+
+    
+
+main:    
+
+    jal proc
+
+    
+
+    jr $ra 
---
+++ 2º_ano/1º_semestre/AC-I/Teste_Exemplo2/ex3.asm
@@ -0,0 +1,70 @@
+#double prcells(int size, t_cell *tc) {
+#    double tmp, result = 1.0;
+#    int i;
+#
+#    for (i = 0; i < size; i++) {
+#        tmp = (double)tc[i].ns / 3.597;
+#        result += tmp;
+#        tc[i].xpt = tmp;
+#        tc[i].sum = (int)result - 1;
+#    }
+#    return result;
+#}
+
+# Copie a estrutura para o topo da área de resposta  
+# e substitua xx pelo valor adequado
+# typedef struct 
+# {                  Alignment  Size   Offset
+#    char smp[17];      1 	17     0
+#    double xpt;        8         8      17 -> 24
+#    int ns;            4         4      32
+#    char id;           1         1      36 
+#    int sum;           4         4      37-> 40
+#} t_cell       max(1,4,8) = 8   44      48 
+
+# double prcells( int size -> $a0, t_cell *tc -> $a1)
+# tmp : $f2
+# result : $f4
+# i : $t0
+# &tc[i] : $t1
+
+
+	.data
+result:	.double 1.0
+d:	.double 3.597
+	.text
+	.globl prcells
+	
+prcells:	
+	la $t0, result		#
+	l.d $f4, 0($t0)		# double result = 0.0
+	
+	li $t0, 0		# int i = 0
+	
+for:	bge $t0, $a0, endfor
+	mulu $t1, $t0, 48
+	addu $t1, $a1, $t1	# &tc[i]
+	lw $t2, 32($t1)		# tc[i].ns
+	
+	mtc1 $t2, $f6		
+	cvt.d.w $f6, $f6		# (double) tc[i].ns 
+	
+	la $t3, d
+	l.d $f8, 0($t3)
+	div.d $f2, $f6, $f8	# tmp = (double) tc[i].ns / 3.597
+	
+	add.d $f4, $f4, $f2	# result += tmp
+	
+	s.d $f2, 24($t1)		# tc[i].xpt = tmp
+	
+	cvt.w.d $f10, $f4	# 
+	mfc1 $t4, $f10		#
+	addi $t4, $t4, -1	# (int) result - 1
+	sw $t4, 40($t1)		# tc[i].sum = (int) result
+	
+	addi $t0, $t0, 1 	# i++	
+	j for
+endfor:	
+	mov.d $f0, $f4
+	
+	jr $ra
