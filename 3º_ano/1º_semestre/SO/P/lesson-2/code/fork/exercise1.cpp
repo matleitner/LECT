21260ab6a10ec25eab395187a59343dee91548e8
matleitner <testembl04@gmail.com>
2026-09-22 14:16:40 +0100

    SO

---
+++ 3º_ano/1º_semestre/SO/P/lesson-2/code/fork/exercise1.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "delays.h"
+#include "process.h"
+
+int main(void) {
+  printf("Before the fork: PID = %d, PPID = %d\n", getpid(), getppid());
+  pid_t ret =
+      pfork(); // equivalent to fork(), dealing internally with error situations
+  if (ret == 0) {
+    bwRandomDelay(100, 100000);
+    printf("After the fork, in the child: PID = %d, PPID = %d\n", getpid(),
+           getppid());
+    for (int i = 11; i <= 20; i++) {
+      printf("%d\n", i);
+    }
+
+  } else {
+    bwRandomDelay(100, 100000);
+    printf("After the fork, in the parent: PID = %d, PPID = %d\n", getpid(),
+           getppid());
+    for (int i = 0; i <= 10; i++) {
+      printf("%d\n", i);
+    }
+    pwait(NULL); // equivalent to wait(.), dealing internally with error
+    // situations
+  }
+
+  return EXIT_SUCCESS;
+}
---
+++ 3º_ano/1º_semestre/SO/P/lesson-2/code2/fork1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "delays.h"
+#include "process.h"
+
+int main(void) {
+  printf("Before the fork: PID = %d, PPID = %d\n", getpid(), getppid());
+
+  pfork(); // equivalent to fork(), dealing internally with error situations
+
+  printf("After the fork: PID = %d, PPID = %d\n", getpid(), getppid());
+  bwRandomDelay(
+      10000, 10000000); // added to enhance the occurrence of different outputs
+  printf("  Was I printed by the parent or by the child process? How can I "
+         "know it?\n PID= %d, PPID= %d\n",
+         getpid(), getppid());
+
+  return EXIT_SUCCESS;
+}
